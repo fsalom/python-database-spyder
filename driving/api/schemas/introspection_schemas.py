05bfa87bc8a58bc9de99730bf568289dbc6e12e7
Fernando Salom <fer@rudo.es>
2025-10-05 11:54:49 +0200

    Update introspection_schemas.py

--- driving/api/schemas/introspection_schemas.py
+++ driving/api/schemas/introspection_schemas.py
@@ -15,6 +15,8 @@ class DiscoveredColumnResponse(BaseModel):
     is_nullable: bool
     is_primary_key: bool
     is_foreign_key: bool
+    foreign_key_table: Optional[str] = None
+    foreign_key_column: Optional[str] = None
     default_value: Optional[str] = None
     max_length: Optional[int] = None
     precision: Optional[int] = None
